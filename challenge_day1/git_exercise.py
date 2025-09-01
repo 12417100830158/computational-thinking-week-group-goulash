153539dcb20ceca97390b8be19b07185247366f2
former-tippetarius <mr.ritistr@gmail.com>
2025-09-01 15:43:18 +0200

    Update git_exercise.py

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,6 +1,6 @@
-import mai, maria, Rita, sem
+import mai, maria, Rita, sem, juan
 
 def weare ():
-    return f"This is Team gulash. We are: {mai.printname()}, {maria.printname()}, {Rita.get_name()}, {sem.get_name()}"
+    return f"This is Team gulash. We are: {mai.printname()}, {maria.printname()}, {Rita.get_name()}, {sem.get_name()}, {juan.get_name()}"
 
 print(weare())
