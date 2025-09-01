a8cac7ff37076eceb1851f47b02ba52b4ef739b1
Queso <sem.jong@student.uva.nl>
2025-09-01 13:12:16 +0200

    fixes printing

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -2,3 +2,5 @@ import mai, maria, Rita, sem
 
 def weare ():
     return f"This is Team gulash. We are: {mai.printname()}, {maria.printname()}, {Rita.get_name()}, {sem.get_name()}"
+
+print(weare())
