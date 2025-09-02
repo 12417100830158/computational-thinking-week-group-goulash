8c2dd71bb7ebe9e1830335fbf2e313302ffc1ab5
Juan Gutierrez <juan.esteban.gutierrez.ceron@student.uva.nl>
2025-09-02 14:48:59 +0200

    Adds file station5.py

---
+++ challenge_day2/station5.py
@@ -0,0 +1,9 @@
+students = {"Daeho": 1, "David": 1, "Kaisa": 1, "Oliver": 1, "Sara": 1, "Dan": 1, "Ivar": 1, "Lotte": 1, "Riya": 1, "Vassil": 1, "Twan": 1, "Ester": 1, "Karolina": 1, "Lena": 1, "Margarita": 1, "Anna": 1, "Kien": 1, "Klaudia": 1, "Maliah": 1, "Todd": 1,
+            "Oumaima": 2, "Mathilde": 2, "Marie": 2, "Anita": 2, "Ziyan": 2, "Bernardo": 2, "Eleanor": 2, "Lorijn": 2, "Maria": 2, "Younes": 2, "Yvan": 2, "Henning": 2, "Liangyu": 2, "Maciej": 2, "Toprak": 2, "Chris": 2, "GengXin": 2, "Mingze": 2, "Phoebe": 2,
+            "Betija": 3, "Haider": 3, "Kacper": 3, "Sophie": 3, "Amir": 3, "Baltasar": 3, "Isar": 3, "Jelle": 3, "Nicolas": 3, "David": 3, "Ipek": 3, "Juan": 3, "Marfa": 3, "Maria": 3, "Alissa": 3, "Leopoldo": 3, "Mies": 3, "Jiaying": 3, "Kaixin": 3, "Mai": 3, "Sem": 3, "Tibbe": 3,
+            "Justus": 4, "Julia": 4, "Philip": 4, "Uli": 4, "Vanessa": 4, "Anna": 4, "Ekaterina": 4, "Thessa": 4, "Tongfei": 4, "Yang": 4, "Benedikt": 4, "Jan": 4, "Nadee": 4, "Osjah": 4, "Tim": 4, "Eliana": 4, "Joana": 4, "Peilin": 4, "Pija": 4, "Wenhao": 4,
+            "Afua": 5, "Cristina": 5, "Greta": 5, "Jace": 5, "Laura": 5, "Anna": 5, "Bassant": 5, "Ivan": 5, "Juriaan": 5, "Kiavash": 5,
+            'Keitaro': 6, "Nohemi": 6, "Norina": 6, "Yifan": 6, "Yinan": 6, "Luo": 6, "Nikola": 6, "Olesya": 6, "Sophie": 6, "Tom": 6
+            }
+def solution_station_5(name: str):
+    return students[name]
