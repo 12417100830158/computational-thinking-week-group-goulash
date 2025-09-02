5bfad5774a5da02ecdec5df516a4f700f8691f7b
12417100830158 <maimai883m@gmail.com>
2025-09-02 15:51:11 +0200

    Update station3.py

--- challenge_day2/station3.py
+++ challenge_day2/station3.py
@@ -1,5 +1,11 @@
 def solution_station_3(input):
     if input%3 == 0:
+<<<<<<< Updated upstream
         return True
     else:
         return False
+=======
+        print("True")
+    else:
+        print("False")
+>>>>>>> Stashed changes
