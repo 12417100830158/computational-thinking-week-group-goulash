fce497cf4523c8c7343ac544deaff286c34340bc
12417100830158 <maimai883m@gmail.com>
2025-09-02 15:49:58 +0200

    Update station7.py

--- challenge_day2/station7.py
+++ challenge_day2/station7.py
@@ -1,9 +1,9 @@
 def solution_station_7(expr: str) -> float:
     variables = {
-        "a": 3,
-        "b": -1,
-        "c": 4,
-        "d": 7,
+        "a": 3.0,
+        "b": -1.0,
+        "c": 4.0,
+        "d": 7.0,
         "e": 0.5
     }
     return eval(expr, {}, variables)
