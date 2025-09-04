efc4a5d1eda2f7d0d1d6888b4f3ad756996380a2
marialaitinen <ranilait@gmail.com>
2025-09-04 14:40:04 +0200

    i did my part

--- challenge_day4/Julia_step3/sort.jl
+++ challenge_day4/Julia_step3/sort.jl
@@ -1,7 +1,7 @@
 using DataFrames, CSV, Statistics, DelimitedFiles
 
 # Read the CSV file into a DataFrame
-people_df = CSV.File("data3.csv") |> DataFrame
+people_df = CSV.File(raw"testdata\data3.csv") |> DataFrame
 
 # Function to classify a score based on quartiles
 function classify_score(score, quartiles)
@@ -38,7 +38,7 @@ for col_name in names(people_df)[2:end]
 end
 
 # Save the modified DataFrame back to a new CSV file
-CSV.write("data4.csv", people_df)
+CSV.write(raw"testdata\data4.csv", people_df)
 
 # Optionally also save as TXT (comma-delimited)
 #writedlm("data4.txt", Tables.columntable(people_df), ',')
